f209e4eab2a352f27b2ff7ff815406ef1dfd56e4
Mack Case <hcase@smith.edu>
2024-02-16 15:41:31 -0500

    created class Engine

--- Engine.java
+++ Engine.java
@@ -1,3 +1,18 @@
 public class Engine {
+    double maxFuel;
+    double currentFuel;
+    FuelType fuelType;
 
+    public Engine(FuelType f, double mFuel, double cFuel){
+        this.fuelType = f;
+        this.maxFuel = mFuel;
+        this.currentFuel = cFuel;
+        }
+    }
+//do same below for each file
+    public static void main(String [] args){
+        Engine newEngine = new Engine(FuelType.STEAM, 100.0, 80.0);
+        System.out.println(newEngine.fuelType);
+    }
 }
+
